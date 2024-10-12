091fe5834e0c3437cedd9865a7cd0b0f718b12b8
Tran Duc Anh <tranducanh@sace.edu.vn>
2024-10-12 21:26:40 +1100

    init commit
    working on task 2 (2.3) but still not satisfied

---
+++ A2_MARKING_SCRIPT.sql
@@ -0,0 +1,16 @@
+-- NOTE:  THIS SCRIPT WILL ONLY WORK ON THE SWINBURNE DATABASE
+
+CLEAR SCREEN;
+SET SERVEROUTPUT ON;
+
+exec A2M_CLEAN_UP;
+
+@./Ass2_DW_Creation_Script.SQL;
+ 
+CLEAR SCREEN;
+
+-- changing the second parameter below from false to true 
+-- will cause the marking script to give more verbose output.
+exec A2M_Mark_Ds_Ass2( USER, FALSE );
+
+
